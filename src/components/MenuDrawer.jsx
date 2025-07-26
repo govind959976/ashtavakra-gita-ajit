675b96b366d95c4471f7b08ef151ccb6a3abc1c1
Govind Rawat <govindrawat48831@gmail.com>
2025-07-26 14:51:36 +0530

    Add MenuDraweer

--- src/components/MenuDrawer.jsx
+++ src/components/MenuDrawer.jsx
@@ -13,35 +13,35 @@ const MenuDrawer = ({ onSelect }) => {
   };
 
   return (
-    <div className="absolute right-4 top-16 bg-white dark:bg-white shadow-lg rounded-lg p-3 z-10 space-y-2 w-64 text-sm">
-      <button onClick={handleClick} className="block w-full text-left">
+    <div className="absolute right-4 top-16 bg-white dark:bg-gray-800 shadow-lg rounded-lg p-3 z-10 space-y-2 w-64 text-sm">
+      <button onClick={handleClick} className="block w-full text-left text-white">
         {t.aajKaShlok || "📜 Today's Shloka"}
       </button>
 
-        <button onClick={() => navigate("/progress")} className="block w-full text-left">
+        <button onClick={() => navigate("/progress")} className="block w-full text-left text-white">
                   {t.progress || "📈 Progress Report"}
                 </button>
 
-      <button onClick={() => onSelect("bookmark")} className="block w-full text-left">
+      <button onClick={() => onSelect("bookmark")} className="block w-full text-left text-white">
         {t.bookmarkList || "⭐ Bookmarks"}
       </button>
       <hr />
-      <button onClick={() => onSelect("settings")} className="block w-full text-left">
+      <button onClick={() => onSelect("settings")} className="block w-full text-left text-white">
         {t.settings || "⚙️ Settings"}
       </button>
     
-      {/*<button onClick={() => onSelect("dark")} className="block w-full text-left">
+      {/*<button onClick={() => onSelect("dark")} className="block w-full text-left text-white">
         {t.darkMode || "🌙 Dark Mode"}
       </button>
       */}
       <hr />
-      <button onClick={() => navigate("/contact")} className="block w-full text-left">
+      <button onClick={() => navigate("/contact")} className="block w-full text-left text-white">
         {t.contact || "✉ Contact Us"}
       </button>
-      <button onClick={() => onSelect("about")} className="block w-full text-left">
+      <button onClick={() => onSelect("about")} className="block w-full text- text-white">
         {t.about || "ℹ️ About App"}
       </button>
-      <button onClick={() => onSelect("privacy")} className="block w-full text-left">
+      <button onClick={() => onSelect("privacy")} className="block w-full text-left text-white">
         {t.privacy || "🔐 Privacy Policy"}
       </button>
     </div>
